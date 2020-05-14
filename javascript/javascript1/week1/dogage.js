8d71c8b2452b88b5546e044f98640d7cd8639687
Kiran-Serolla <serollakiran@outlook.com>
2020-05-14 13:11:16 +0200

    Added Dog age calculator

---
+++ javascript/javascript1/week1/dogage.js
@@ -0,0 +1,13 @@
+let dogYearOfBirth = 2017;
+let dogYearFuture = 2027;
+let dogYear = dogYearFuture-dogYearOfBirth;
+
+let shouldShowResultInDogYears = false;
+
+if (shouldShowResultInDogYears == true) 
+{
+    console.log("your dog will be " + dogYear*7 + " dog years old in 2027");
+}
+ else {
+    console.log("Your dog will be " + dogYear+ " human years old in 2027")
+}
