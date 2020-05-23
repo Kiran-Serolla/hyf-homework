97440361faeace498a7ca0478ea8397e9a96c99b
Kiran-Serolla <serollakiran@outlook.com>
2020-05-23 16:55:17 +0200

    fullname function implemented

---
+++ javascript/javascript1/week2/fullnameFunction.js
@@ -0,0 +1,11 @@
+function getFullname(firstname, surname) {
+  return firstname + surname;
+}
+
+let fullname1, fullname2;
+
+fullname1 = getFullname("Kiran ", "Serolla");
+
+fullname2 = getFullname("Kanishk ", "Kalvala");
+console.log(fullname1);
+console.log(fullname2);
